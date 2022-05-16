cce73184d9b797130290ce51e0a0b11875b6e5dd
lior2911 <aytegev1@gmail.com>
2022-05-16 22:11:15 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,162 @@
+// var names = ["lior","daniel","eldad","dawit","shimon"]
+// console.log(names[2])
+// for (var i =0 ; i > names.length;i++){
+//   console.log(names)
+// }
+
+
+// var numbers = [100,200,300,400,500,600,700]
+// // console.log(numbers[3])
+// for (var i = 0; i<numbers.length;i++){
+
+//   console.log(numbers[i])
+// }
+
+// var numbers = [10,13,44,32,24,46,39,27,37,18]
+// for (var i = 0 ;i<numbers.length;i++){
+//   if(numbers[i]%2==0){ 
+//     console.log(numbers[i])
+//   } 
+// }
+
+// var myArray = ["hello",2,"world","jacob",6,7,"bye",98]
+// for (var i = 0 ; i<myArray.length;i++){
+//   if (myArray[i]*1 != myArray[i]){
+//     console.log(myArray[i])
+//   }
+// }
+
+// var array = []
+// array.push(9)
+// console.log(array)
+// for (var i = 0 ; i <10 ;i++){
+//   array.push(i)
+// }
+// console.log(array)
+
+// var sum = 0
+// var numArray = [5,3,7,8,11]
+// for (var i = 0 ; i<numArray.length;i++){
+//    sum += numArray[i]
+  
+//   }
+//   console.log(sum/i)
+//   console.log(sum)
+
+// var namesArrays = ["lior","lior","eldad","shimon","malada"]
+// for(var i = 0; i<namesArrays;i++){
+// }
+// console.log(namesArrays)
+
+// function getNum(even){
+//   var save = 0
+//   for(var i = 0 ; i < even.length;i++){
+//     if(even[i]%2==0){
+//       save++
+//     }
+//   }
+//   console.log(save)
+// }
+// getNum([2,1,3,6,21423,34,64,67,78,69,789,5683,9])
+
+// function getArray (num1,num2){
+//   for(var i = 0 ; i<num1.length;i++){
+//     if (num1[i]==num2){
+//       return true
+//     }
+//   }
+//   return false
+
+// }
+
+// console.log(getArray([12,23,44,53,76,54,],88))
+
+// function getEven(x,y){
+//   for (var i = 0 ; i<x.length ; i++){
+//     if(x.length==y.length){
+//       x=y
+//       console.log(x)
+//       return true
+//     }
+//     return false
+//   } 
+// }
+// console.log(getEven([123,23,43],[234,123,345]))
+
+// function getMin (x){
+//   var max = 0
+//   for(var i = 0 ; i < x.length;i++){
+//     if(x[i]>= max){
+//       max = x[i]
+
+//     }
+//   }
+//   return max
+// }
+// console.log(getMin([23,452,1,3,543,56,232,554]))
+
+// function getMin(x){
+//   var min = x[0]
+//   var minIndex = min[i]
+//   for (var i= 0 ; i<=x.length ; i++){
+//     if (x[i]<= min ){
+//       min = x[i]
+//     }
+//   }
+//   return minIndex
+// }
+// console.log(getMin([423,3546,6553,2,23,34,54,5]))
+
+// function getSum(x){
+//   var sum = 0
+//   for(var i = 0 ; i < x.length ; i++){
+//     sum+=x[i]
+  
+    
+//   }
+//   console.log(sum)
+// }
+// getSum([1,2,3,4])
+
+// function array(x){
+//   var max = 0
+//   var min = x[0]
+//   for(var i = 0 ;i<x.length;i++){
+//     if(max<x[i]){
+//       max = x[i]
+//     }
+//     if(min>x[i]){
+//       min = x[i]
+//     }
+//   }
+//   console.log(max*min)
+// }
+// array([10,3,5,6,7,2])
+
+// function getArray(array){
+//   var even = 0
+//   var max = 0
+//   for (var i = 0;i<array.length;i++){
+//     if (array[i]%2==0){
+//       even = array[i]
+//       max = i
+//     }
+   
+//   }
+//   console.log(even,max)
+// }
+// getArray([2,12,5,18])
+
+function array(odd){
+  var num = 0
+  var oddNum = 0
+  for(var i = 0 ; i < odd.length;i++){
+    if (odd[i]%2==1){
+      num = i
+      oddNum = odd[i]
+    }
+
+  }
+  console.log(oddNum)
+}
+array([1,2,3,4,5,6,7])
